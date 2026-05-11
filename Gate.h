91bf300c62eb9b4b5e660a73f394b93bf75cf1e9
14soomin <050214soomin@naver.com>
2026-05-12 00:24:07 +0900

    Gate.h 1차
    cpp 파일과 수정내용 동일 1차

--- Gate.h
+++ Gate.h
@@ -2,22 +2,25 @@
 #define GATE_H
 
 #include <vector>
-#include "map.h"   
+#include "map.h"
 #include "Snake.h"
 
 class Gate {
 private:
-    Position gateA; // Position은 Snake.h에 정의된 (y, x) 형식을 따름 
+    Position gateA; // Snake의 Position 기준인 (y, x)로 저장됨
     Position gateB;
 
 public:
     Gate();
-
-    //Gate 생성 
+    
+    // 1. 게이트 생성 함수
     void generate(Map& map);
-
-    //이동 처리 및 방향 전환 
+    
+    // 2. 텔레포트 및 진출 방향 결정 함수
     Position teleport(Position currentHead, Direction& currentDirection, Map& map);
+
+    // 3. 뱀이 게이트를 통과 중인지 확인하는 함수 (소멸 방지용)
+    bool isSnakePassing(Map& map, Snake& snake);
 };
 
 #endif

e01ccdc419ac8353b081d7eec2976df08c85c2ef
D42-Skyfield <124449676+D42-Skyfield@users.noreply.github.com>
2026-05-11 13:39:24 +0900

    item.cpp 코드 수정
    멤버 변수 2개 추가 선언, 파라미터 변경, 구현내용 변경

--- item.cpp
+++ item.cpp
@@ -14,6 +14,8 @@ class Item{
     private:
     vector<Position> growthItems; //만약 queue로 구현 가능하다면 queue로 변경
     vector<Position> poisonItems; //queue로 변경 가능하다면 queue로 변경
+    vector<unsigned int> growthItemTime;
+    vector<unsigned int> poisonItemTime;
 
     public:
     /*
@@ -26,12 +28,6 @@ class Item{
     */
 
     Item(){
-        vector<Position> growthItems; //growth 아이템의 위치 목록
-        vector<Position> poisonItems; //poison 아이템의 위치 목록
-        /*각 아이템별 설치 시간에 관한 정보를 따로 관리할 수 있는 컨테이너가 있으면 좋을 듯
-            vector<unsigned int> growthItemTime;
-            vector<unsigned int> poisonItemTime;
-        */
     }
 
     void spawnGrowth(Map& map);
@@ -44,21 +40,20 @@ class Item{
     int checkCollision(Position head);
 
     void spawnGrowth(Map& map){ //Growth item spawning
-        if(growthItems.size() + poisonItems.size() <= 3)//필드 내 동시에 존재 가능한 아이템 숫자가 3개로 제한되어 있으므로
+        if(growthItems.size() + poisonItems.size() <= 3)//필드 내 동시에 존재 가능한 아이템은 3개로 제한되어 있으므로
         {
+            int x, y;
             do{ 
                 srand((unsigned int)time(NULL)); //time값으로 시드값 초기화
                 //X, Y 랜덤 좌표 생성
-                int x = rand() % width; //map의 멤버변수 width와 height에 접근이 불가능해
-                int y = rand() % height; //이를 참조할 수 있는 메서드가 있으면 좋겠음
+                x = rand() % map.getWidth();
+                y = rand() % map.getHeight();
             }
             while(!map.isInside(x, y) || !map.isEmpty(x, y));//올바른 좌표가 나올때까지 좌표 생성 반복
 
             map.setCell(x, y, 5); //맵의 해당 위치에 아이템 배치
-            growthItems.push_back({x, y}); //growthItem 위치를 벡터에 추가(큐로 바뀔 경우 enqueue 형식으로 변경)
-            /*시간 정보 컨테이너에 생성 시간을 push
-            growthItemTime.push((unsigned int)time(NULL));
-            */
+            growthItems.push_back({x, y}); //growthItem 위치를 벡터에 추가
+            growthItemTime.push_back((unsigned int)time(NULL)); //시간 정보 컨테이너에 생성 시간을 push
             return;
         }
     }
@@ -66,28 +61,43 @@ class Item{
     void spawnPoison(Map& map){
         //형식은 spawnGrowth와 동일하되 일부 변수명과 상수값 변경이 필요
     }
-    void removeOldItems(){ //파라미터로 Map 타입의 &map을 받아 맵에 접근해야 아이템을 삭제할 수 있으므로 파라미터 변경에 대해 제안할 필요가 있음.
+    void removeOldItems(Map& map){
         unsigned int now = (unsigned int)time(NULL);
-        for(int i = 0; i < 3; i++){ //for문으로 시간 정보 컨테이너를 순회하며
-            if(now >= timeContainer[i] + 5){
-                growthItems //시간이 지난 아이템 제거
+        for(int i = 0; i != growthItems.size(); i++){ //for문으로 시간 정보 컨테이너를 순회하며
+            if(now >= growthItemTime[i] + 5){
+                //해당하는 아이템의 위치정보를 참조해 map에 접근해 아이템을 빈 공간으로 변경한다.
+                growthItems.erase(growthItems.begin() + i); //시간이 지난 아이템 위치정보 제거
+                growthItemTime.erase(growthItemTime.begin() + i); //시간이 지난 아이템 시간정보 제거
+                i--; //erase()를 실행하면 뒤의 원소들이 한칸씩 당겨지므로 i를 1 감소시킴
+            }
+        }
+        for(int i = 0; i != poisonItems.size(); i++){
+            if(now >= poisonItemTime[i] + 5){
+                poisonItems.erase(poisonItems.begin() + i);
+                poisonItemTime.erase(poisonItemTime.begin() + i);
+                i--; //erase()를 실행하면 뒤의 원소들이 한칸씩 당겨지므로 i를 1 감소시킴
             }
         }
     }
 
-    int checkCollision(Position head){ //head는 뱀의 머리 위치임. snake의 move 구현에 따라 세부 구현을 확정짓고자 함.
+    int checkCollision(Position head){//move의 상세 구현에 따라 코드가 수정될 수 있음
         int x = head.first;
         int y = head.second;
-        switch(Map::getCell(x, y)){
-            case 4:
-            case 5: //growth 아이템을 만난 경우
-                snake::grow();
-                Map::setCell(x, y, 3);
-            case 6: //poison 아이템을 만난 경우
+        for(int i = 0; i != growthItems.size(); i++){ //for문으로 위치 정보 컨테이너를 순회하며
+            if(head == growthItems[i]){//머리의 위치와 growth 아이템의 위치가 같으면
+                snake::growth(); //뱀의 길이를 1 증가시키고
+                growthItems.erase(growthItems.begin() + i); //소멸된 아이템 위치정보 제거
+                growthItemTime.erase(growthItemTime.begin() + i); //소멸된 아이템 시간정보 제거
+                i--; //erase()를 실행하면 뒤의 원소들이 한칸씩 당겨지므로 i를 1 감소시킴
+            }
+        }
+        for(int i = 0; i != poisonItems.size(); i++){
+            if(head == poisonItems[i]){
                 snake::shrink();
-                Map::setCell(x, y, 3);
-            case 7:
-
+                poisonItems.erase(poisonItems.begin() + i);
+                poisonItemTime.erase(poisonItemTime.begin() + i);
+                i--;
+            }
         }
     }
-}
+};
